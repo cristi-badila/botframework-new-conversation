f779d5df153f163e0236f68ab967a361fb7475b5
Jason Sowers <v-jassow@microsoft.com>
2018-07-06 15:26:28 -0700

    module changes

--- SurveyModule.cs
+++ SurveyModule.cs
@@ -1,4 +1,6 @@
-﻿namespace CreateNewConversationBot
+﻿using Microsoft.Bot.Builder.Azure;
+
+namespace CreateNewConversationBot
 {
     using Autofac;
     using Microsoft.Bot.Builder.Dialogs;
@@ -12,12 +14,31 @@
         {
             base.Load(builder);
 
+            var store = new InMemoryDataStore();
+
+            builder.Register(c => store)
+                .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                .AsSelf()
+                .SingleInstance();
+
+            builder.Register(c => new CachingBotDataStore(store,
+                    CachingBotDataStoreConsistencyPolicy
+                        .ETagBasedConsistency))
+                .As<IBotDataStore<BotData>>()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+
+
             builder.RegisterType<CreateNewConversationDialog>().As<IDialog<object>>().InstancePerDependency();
 
             builder.Register((c, p) => new SurveyDialog()).AsSelf().InstancePerDependency();
-            builder.RegisterType<SurveyScheduler>().Keyed<ISurveyScheduler>(FiberModule.Key_DoNotSerialize).AsImplementedInterfaces().SingleInstance();
 
-            builder.Register(c => new SurveyService(c.Resolve<ISurveyScheduler>(), c.Resolve<ConversationReference>())).Keyed<ISurveyService>(FiberModule.Key_DoNotSerialize).AsImplementedInterfaces().InstancePerMatchingLifetimeScope(DialogModule.LifetimeScopeTag);
+            builder.RegisterType<SurveyScheduler>().Keyed<ISurveyScheduler>(FiberModule.Key_DoNotSerialize)
+                .AsImplementedInterfaces().SingleInstance();
+
+            builder.Register(c => new SurveyService(c.Resolve<ISurveyScheduler>(), c.Resolve<ConversationReference>()))
+                .Keyed<ISurveyService>(FiberModule.Key_DoNotSerialize).AsImplementedInterfaces()
+                .InstancePerMatchingLifetimeScope(DialogModule.LifetimeScopeTag);
         }
     }
 }
